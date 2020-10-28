fa9538e8e78511e84bef0c696e2f071e7a9f31c6
Andres <theprofessordre@gmail.com>
2020-10-28 11:37:37 -0400

    arreglo en diseño responsive

--- app/sidebar.js
+++ app/sidebar.js
@@ -6,3 +6,27 @@ const showSidebar = ()=>{
     document.querySelector(".sidebar").classList.toggle("animate__zoomInUp")
 }
 btnBurger.addEventListener("click", showSidebar)
+
+class BarraHeader{
+
+    constructor(selector) {
+        
+        this.eventListeners()
+    }
+
+    eventListeners(){
+        document.addEventListener("scroll", this.animateScroll)
+    }
+
+    animateScroll(){
+        if (scrollY > 300) {
+            document.querySelector(".barra-header").classList.add("animate-barra")
+        }else{
+            document.querySelector(".barra-header").classList.remove("animate-barra")
+        }
+    }
+}
+
+(()=>{
+    new BarraHeader
+})()
